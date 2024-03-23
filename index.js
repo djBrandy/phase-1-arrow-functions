ed010ac9db468db059110b056d740f3e92d6062b
djBrandy <dandobrandon0@gmail.com>
2024-03-23 03:01:55 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function () {
+    let a = 2000;
+    let b =100;
+    return a/b;
+
+}
+let x = 2
+const square = (x) => x ** 2;
+
+let a = 3
+let b = 4
+const add = (num) => a + b;
